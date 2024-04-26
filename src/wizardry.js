f9615d91511e4df81ba53e1ee8bd9b5b3c4b2e30
Jian <jianxi.chen8888@gmail.com>
2024-04-26 17:33:16 -0400

    heightmap

--- src/wizardry.js
+++ src/wizardry.js
@@ -3,33 +3,69 @@ import * as BABYLON from "@babylonjs/core";
 const canvas = document.getElementById("main-scene");
 const engine = new BABYLON.Engine(canvas);
 
-const createScene = function () {
-  const scene = new BABYLON.Scene(engine);
-  scene.createDefaultCameraOrLight(true, false, true);
+const createScene = async () => {
+	const scene = new BABYLON.Scene(engine);
+	scene.createDefaultCameraOrLight(true, false, true);
 
-  const sphere = new BABYLON.MeshBuilder.CreateSphere(
-    "aSphere",
-    {
-      segments: 1,
-      diameter: 0.1,
-    },
-    scene
-  );
+	/* Regular shape meshs */
+	// const sphere = new BABYLON.MeshBuilder.CreateSphere(
+	// 	"aSphere",
+	// 	{
+	// 		segments: 1,
+	// 		diameter: 0.1,
+	// 	},
+	// 	scene
+	// );
+	// const box = new BABYLON.MeshBuilder.CreateBox("aBox", {
+	//   size: 0.1,
+	//   width: 2,
+	//   faceColors: [new BABYLON.Color4(1,0,0,1)]
+	// }, scene);
 
-  // const box = new BABYLON.MeshBuilder.CreateBox("aBox", {
-  //   size: 0.1,
-  //   width: 2,
-  //   faceColors: [new BABYLON.Color4(1,0,0,1)]
-  // }, scene);
+	/* Ground meshs */
+	// const groundHeightMap = new BABYLON.MeshBuilder.CreateGroundFromHeightMap(
+	// 	"hMap",
+	// "/public/assets/city_height_map.png",
+	// {
+	//   height: 10,
+	//   width: 10,
+	//   subdivisions: 500
+	// }
+	// );
+	// groundHeightMap.material = new BABYLON.StandardMaterial();
+	// groundHeightMap.material.wireframe = true;
 
-  return scene;
+	/* Text */
+	const spellFont = await (await fetch("/public/assets/First_Order_Plain_Regular.json")).json();
+	const sampleText = new BABYLON.MeshBuilder.CreateText(
+		"aText",
+		"Textual text",
+		spellFont,
+		{
+			size: 2,
+		}
+	);
+
+	return scene;
 };
 
-const mainScene = createScene();
-engine.runRenderLoop(function () {
-  mainScene.render();
-});
+// const mainScene = createScene().then(() => {
+// 	engine.runRenderLoop(() => {
+// 		mainScene.render();
+// 	});
+// }, (err) => {
+// 	console.log(err);
+// })
 
-window.addEventListener("resize", function () {
-  engine.resize();
+var scene = createScene();
+scene.then((scene)=>{
+	engine.runRenderLoop(function () {
+		if (scene) {
+			scene.render();
+		}
+	});
+})
+
+window.addEventListener("resize", () => {
+	engine.resize();
 });
