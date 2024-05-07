86f3b1a1ae75ba47e11749f803a6cd75f038e2af
Jian <jianxi.chen8888@gmail.com>
2024-05-07 01:19:50 -0400

    move to convention of index.js in each folder

--- src/SushiOrder/index.ts
+++ src/SushiOrder/index.ts
@@ -3,29 +3,27 @@ import "@babylonjs/loaders/glTF";
 import "@babylonjs/loaders/OBJ";
 import "@babylonjs/core/Debug/debugLayer";
 
-
-const canvas = <HTMLCanvasElement> document.getElementById("main-scene");
+const canvas = <HTMLCanvasElement>document.getElementById("main-scene");
 const engine = new BABYLON.Engine(canvas);
 
 // Loading screen
-
-function CustomLoadingScreen( /* variables needed, for example:*/ text) {
-    //init the loader
-    this.loadingUIText = text;
-  }
-  CustomLoadingScreen.prototype.displayLoadingUI = function() {
-    alert(this.loadingUIText);
-  };
-  CustomLoadingScreen.prototype.hideLoadingUI = function() {
-    alert("Loaded!");
-  };
-
+function CustomLoadingScreen(/* variables needed, for example:*/ text) {
+	//init the loader
+	this.loadingUIText = text;
+}
+CustomLoadingScreen.prototype.displayLoadingUI = function () {
+	alert(this.loadingUIText);
+};
+CustomLoadingScreen.prototype.hideLoadingUI = function () {
+	alert("Loaded!");
+};
+engine.loadingScreen = new CustomLoadingScreen("loading");
 engine.displayLoadingUI();
 
 const createScene = async () => {
 	const scene = new BABYLON.Scene(engine);
 	scene.createDefaultCameraOrLight(true, false, true);
-    engine.hideLoadingUI();
+	engine.hideLoadingUI();
 	return scene;
 };
 
